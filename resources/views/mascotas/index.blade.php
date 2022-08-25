6fa910830bece315d500fc1765096c9718d6f268
Jhordy0712 <santyrs712@gmail.com>
2022-08-24 20:06:31 -0500

    titulo

--- resources/views/mascotas/index.blade.php
+++ resources/views/mascotas/index.blade.php
@@ -6,7 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>mascotass</title>
+
+
+
+    <title>mascotass titulo</title>
     <title>mascotass</title>
 </head>
 <body>
